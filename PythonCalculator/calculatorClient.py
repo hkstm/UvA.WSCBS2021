9fbde9bda70ce38a960afc877d98f3ad192d2632
yb123tcs <yb@123tcs.io>
2021-04-06 16:44:33 +0200

    PythonCalculator

---
+++ PythonCalculator/calculatorClient.py
@@ -0,0 +1,46 @@
+import zeep
+from zeep import Client
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+c = zeep.Client('http://localhost:8000/?wsdl')
+
+
+@app.route('/')
+def main():
+    return render_template("app.html")
+
+
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = request.form['num1']
+    num2 = request.form['num2']
+    operation = request.form['operation']
+    if operation == 'add':
+       
+        result = str(c.service.Add(num1, num2))       
+        return render_template('app.html', result=result)
+
+    elif operation == 'subtract':
+        result = str(c.service.Subtract(num1, num2))
+        return render_template('app.html', result=result)
+
+    elif operation == 'multiply':
+        result = str(c.service.Multiply(num1, num2))
+        return render_template('app.html', result=result)
+        
+    elif operation == 'divide':
+        result = str(c.service.Div(num1, num2))
+        return render_template('app.html', result=result)
+    else:
+        return render_template('app.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+  
---
+++ PythonCalculator/calculatorService.py
@@ -0,0 +1,51 @@
+from spyne import Application, rpc, ServiceBase, Iterable, Integer, Unicode, Double
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
+
+
+
+class CalculatorService(ServiceBase):
+    @rpc(Double, Double, _returns=Double)      
+    def Add(self, left, rigth):
+        total = left + rigth
+        return total
+
+
+    @rpc(Integer, Integer, _returns=Integer)      
+    def Subtract(self, left, rigth):
+        total = left - rigth
+        return total
+
+    @rpc(Integer, Integer, _returns=Integer)      
+    def Multiply(self, left, rigth):
+        total = left * rigth
+        return total
+
+    @rpc(Integer, Integer, _returns=Integer)      
+    def Divide(self, left, rigth):
+        total = left / rigth
+        return total
+
+
+
+application = Application([CalculatorService], 'spyne.examples.hello.soap',
+                          in_protocol=Soap11(validator='lxml'),
+                          out_protocol=Soap11())
+
+wsgi_application = WsgiApplication(application)
+
+
+
+if __name__ == '__main__':
+    import logging
+
+    from wsgiref.simple_server import make_server
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
+
+    logging.info("listening to http://127.0.0.1:8000")
+    logging.info("wsdl is at: http://localhost:8000/?wsdl")
+
+    server = make_server('127.0.0.1', 8000, wsgi_application)
+    server.serve_forever()
